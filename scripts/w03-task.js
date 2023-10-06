ac8fbb2c96408fa69e6ad2144a558777ea89f737
jaredlopez33 <jared.richard.2005@gmail.com>
2023-10-05 23:16:33 -0500

    week04

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -5,7 +5,7 @@
 function add(number1, number2) {
     return number1 + number2;
 }
-function addNumbers(){
+function addNumbers() {
     let addNumber1 = Number(document.querySelector("#add1").value);
     let addNumber2 = Number(document.querySelector("#add2").value);
     document.querySelector("#sum").value = add(addNumber1, addNumber2);
@@ -14,10 +14,10 @@ function addNumbers(){
 document.querySelector("#addNumbers").addEventListener("click", addNumbers);
 
 /* Function Expression - Subtract Numbers */
-function subtract(number1, number2){
+function subtract(number1, number2) {
     return number1 - number2;
 }
-function subtractNumbers(){
+function subtractNumbers() {
     let subtractNumber1 = Number(document.querySelector("#subtract1").value);
     let subtractNumber2 = Number(document.querySelector("#subtract2").value);
     document.querySelector("#difference").value = subtract(subtractNumber1, subtractNumber2);
@@ -26,23 +26,23 @@ function subtractNumbers(){
 document.querySelector("#subtractNumbers").addEventListener("click", subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
-multiply = (number1, number2) => number1 * number2; 
+multiply = (number1, number2) => number1 * number2;
 
 multiplyNumbers = () => {
     let factor1 = Number(document.querySelector("#factor1").value);
     let factor2 = Number(document.querySelector("#factor2").value);
     document.querySelector("#product").value = multiply(factor1, factor2);
-} 
+}
 
 document.querySelector("#multiplyNumbers").addEventListener("click", multiplyNumbers);
 
 
 /* Open Function Use - Divide Numbers */
-function divide(number1, number2){
+function divide(number1, number2) {
     return number1 / number2;
 }
 
-function divideNumbers(){
+function divideNumbers() {
     let dividend = Number(document.querySelector("#dividend").value);
     let divisor = Number(document.querySelector("#divisor").value);
     document.querySelector("#quotient").value = divide(dividend, divisor);
